b8a2ea825f1f069f270fe69a495a8d5b1e4ac951
lannie-tran <77120453+lannie-tran@users.noreply.github.com>
2021-02-25 11:02:10 -0800

    lab8

---
+++ public/js/introHCI.js
@@ -0,0 +1,38 @@
+// 'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Page ready");
+ 	// initCamera();
+ 	initMap();
+ 	initGestures();
+ 	initRSVPForm();
+}
+
+// init jQuery gestures  
+function initGestures() {
+	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			//get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+			//show bio
+			$("#" + targetIDPrefix + "-bio").show()
+		}
+	});
+}
+
+// init RSVP form submit listener
+function initRSVPForm() {
+  // add your code here
+}
